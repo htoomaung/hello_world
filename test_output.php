96fb341ef6638fdd5dd006bfed4938be289ed8b4
Htoo Maung Thait <robinhtoo@gmail.com>
2016-07-02 22:31:05 +0630

    New array to json output

--- test_output.php
+++ test_output.php
@@ -1,4 +1,6 @@
 <?php 
 	$output_string = "Hello World!";
+	$output = ["Alpha Romeo", "Audi", "Chuvlet", "Astonmartin", "Labogani"];
 	echo $output_string;
+	echo json_encode($output, JSON_PRETTY_PRINT);
 ?>
